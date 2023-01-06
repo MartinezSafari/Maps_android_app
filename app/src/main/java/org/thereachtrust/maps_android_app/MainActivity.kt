f349f18a46a33e999b996550ff840c988cc528f7
martinez <martinez@thereachtrust.org>
2023-01-06 23:05:53 +0200

    Initial commit

---
+++ app/src/main/java/org/thereachtrust/maps_android_app/MainActivity.kt
@@ -0,0 +1,25 @@
+package org.thereachtrust.maps_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback
+{
+    lateinit var myMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        myMap = gMap
+    }
+}
